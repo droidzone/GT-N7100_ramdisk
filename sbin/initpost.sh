216b9d8cbdc404aa8adaa1d334e5956fb465c595
Droidzone <dz@droidzone.in>
2012-11-30 19:16:38 +0530

    Update sbin/initpost.sh
    Add permissions for execute in case user hasnt done it.

--- sbin/initpost.sh
+++ sbin/initpost.sh
@@ -4,6 +4,7 @@
 if cd /system/etc/init.d >/dev/null 2>&1 ; then
     for file in * ; do
         if ! cat "$file" >/dev/null 2>&1 ; then continue ; fi
+        chmod 755 "$file"
         /system/bin/sh "$file"
     done
 fi
